35a86ef406e79b98c8e112ca083edc51b60b7158
MazidulIslam <mazidul.islam@prospecta.com>
2023-09-27 18:19:47 +0530

    initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyALdeOhm4sN4e4rTUF-0QGPJXMhOl7f_Bo",
+        authDomain: "gs-opensolutions.firebaseapp.com",
+        projectId: "gs-opensolutions",
+        databaseURL: "https://gs-opensolutions-default-rtdb.asia-southeast1.firebasedatabase.app/",
+        storageBucket: "gs-opensolutions.appspot.com",
+        messagingSenderId: "968374611929",
+        appId: "1:968374611929:web:f54174c1667967947433c8",
+        measurementId: "G-HKFBYXV5WL",
+    },
+};
